be328d3ab9397129bc0c1caf99ae4d73347a693a
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2026-04-21 00:11:58 +0900

    Update api_client.dart

--- yui_app/lib/core/api_client.dart
+++ yui_app/lib/core/api_client.dart
@@ -4,8 +4,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
-// APIのベースURL
-// 本番デプロイ時は実際のWorkers URLに変更してください
 const _baseUrl = 'https://yui-api.yahiro-t-eng.workers.dev';
 
 // トークンを保存するためのキー
